355128a5c61e5b58bc88cf5c58713fb57267f66a
Eshwar <eshwarmp02@gmail.com>
2026-02-24 19:58:34 +0530

    User input is done

---
+++ 01_Basics/Input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Input {
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        System.out.println(user.nextLine());
+        user.close();
+    }
+}
