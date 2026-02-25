eee02f36b9d7cb4494244f99145b35c79d7a4df2
Eshwar <eshwarmp02@gmail.com>
2026-02-25 07:30:18 +0530

    Primitive Datatypes

---
+++ 01_Basics/Primitive.java
@@ -0,0 +1,16 @@
+public class Primitive {
+    public static void main(String[] args) {
+        int number = 51;
+        char letter  = 'a';
+        float decimal = 45.3f;
+        double longDecimalNumber = 324532.343;
+        long largeInteger = 234522345;
+        boolean check = true;
+        System.out.println(number);
+        System.out.println(letter);
+        System.out.println(decimal);
+        System.out.println(longDecimalNumber);
+        System.out.println(largeInteger);
+        System.out.println(check);
+    }
+}
