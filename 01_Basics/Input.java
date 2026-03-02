81fd0fcb2ce20ba31666a3a54dc96aeecf20a751
Eshwar <eshwarmp02@gmail.com>
2026-03-02 21:23:02 +0530

    Input file modified

--- 01_Basics/Input.java
+++ 01_Basics/Input.java
@@ -4,6 +4,9 @@ public class Input {
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
         System.out.println(user.nextLine());
+        System.out.println("Enter the roll number: ");
+        int rollno = user.nextInt();
+        System.out.println("The rollno is "+rollno);
         user.close();
     }
 }
