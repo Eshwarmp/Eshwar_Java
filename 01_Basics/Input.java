5ee4a3fc1dd207bfbd21cf952c6cac72a747618d
Eshwar <eshwarmp02@gmail.com>
2026-03-03 19:32:32 +0530

    Some more info added

--- 01_Basics/Input.java
+++ 01_Basics/Input.java
@@ -6,7 +6,14 @@ public class Input {
         System.out.println(user.nextLine());
         System.out.println("Enter the roll number: ");
         int rollno = user.nextInt();
-        System.out.println("The rollno is "+rollno);
-        user.close();
+        System.out.println("The rollno is " + rollno);
+        int a = 322_000_33;
+        System.out.println(a);
+        System.out.println("Enter your name");
+        user.nextLine(); // bcz nextint() only takes one integer and leaves the newline character
+        // which eventually skips the next line hence we are adding one extra nextline() to avoid the case
+        String name = user.nextLine();
+        System.out.println(name);
+        
     }
 }
