40ea8d97710eefedf2483fe6e95c1d5ab50a0ab4
Eshwar <eshwarmp02@gmail.com>
2026-04-14 12:44:52 +0530

    First array file

--- Arrays/InputArray.java
+++ Arrays/InputArray.java
@@ -2,7 +2,7 @@ package Arrays;
 
 import java.util.Arrays;
 import java.util.Scanner;
-public class Input {
+public class InputArray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = new int[5];
