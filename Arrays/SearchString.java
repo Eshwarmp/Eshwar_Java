90c3243b5bc3b9866be1406ac2a18598dded043c
Eshwar <eshwarmp02@gmail.com>
2026-04-21 22:36:49 +0530

    Searching for specific character in the string

---
+++ Arrays/SearchString.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class SearchString {
+    public static void main(String[] args) {
+        String name = "Eshwar";
+        char target = 'h';
+        int index = search(name, target);
+        char[] arr = name.toCharArray();
+        char key = searchArray(arr, target);
+        System.out.println("The key character "+ key + " is present in the array");
+        System.out.println(Arrays.toString(arr));
+        System.out.println("The character present at the index "+index);
+    }
+
+    static int search(String name, char target) {
+        for (int i = 0; i < name.length(); i++) {
+            if (target == name.charAt(i)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static char searchArray(char[] arr, char target) {
+        for (char key : arr) {
+            if (key == target) {
+                return key;
+            }
+        }
+        return '\0';
+    }
+}
