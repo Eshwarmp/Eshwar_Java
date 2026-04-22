e1ba871140459003d3353248c28f2d664660366d
Eshwar <eshwarmp02@gmail.com>
2026-04-22 23:18:20 +0530

    Alternative best way to count number of digits in an element

--- Arrays/evenDigits.java
+++ Arrays/evenDigits.java
@@ -3,6 +3,14 @@ package Arrays;
 public class evenDigits {
     public static void main(String[] args) {
         int[] arr = { 12, 10, 50, 198 };
+        int even=0;
+        for (int n : arr) {
+            int count = (int) (Math.log10(n)+1); // Alternative way
+            if (count % 2 == 0) {
+                even++;
+            }
+        }
+        System.out.println("The number of elements containing even number of digits is "+ even);
         int noOfEvenDigits = evenNoDigits(arr);
         System.out.println("The number of  elements containing even number of digits is "+ noOfEvenDigits);
     }
@@ -10,6 +18,7 @@ public class evenDigits {
     static int evenNoDigits(int[] arr) {
         int even = 0;
         for (int num : arr) {
+            
             int count = 0;
             while (num > 0) {
                 int digit = num % 10;
