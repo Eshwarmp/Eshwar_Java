68d7e0ba11c0c0e27221646eb625c162b08bd5fc
Eshwar <eshwarmp02@gmail.com>
2026-04-22 22:52:56 +0530

    Return no of elements containing even number of digits

---
+++ Arrays/evenDigits.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class evenDigits {
+    public static void main(String[] args) {
+        int[] arr = { 12, 10, 50, 198 };
+        int noOfEvenDigits = evenNoDigits(arr);
+        System.out.println("The number of  elements containing even number of digits is "+ noOfEvenDigits);
+    }
+
+    static int evenNoDigits(int[] arr) {
+        int even = 0;
+        for (int num : arr) {
+            int count = 0;
+            while (num > 0) {
+                int digit = num % 10;
+                count++;
+                num /= 10;
+            }
+            if (count % 2 == 0) {
+                even++;
+            }
+        }
+        return even;
+    }
+}
