9c4dc17d42a6ad73fbce7f289a7482f9e8bd9c42
Eshwar <eshwarmp02@gmail.com>
2026-04-22 19:55:29 +0530

    Finding a character in particular range

--- Arrays/SearchString.java
+++ Arrays/SearchString.java
@@ -5,13 +5,15 @@ import java.util.Arrays;
 public class SearchString {
     public static void main(String[] args) {
         String name = "Eshwar";
-        char target = 'h';
+        char target = 'r';
         int index = search(name, target);
         char[] arr = name.toCharArray();
         char key = searchArray(arr, target);
         System.out.println("The key character "+ key + " is present in the array");
         System.out.println(Arrays.toString(arr));
-        System.out.println("The character present at the index "+index);
+        System.out.println("The character present at the index " + index);
+        int range = searchRange(name, target);
+        System.out.println("The character is present at the index "+ range);
     }
 
     static int search(String name, char target) {
@@ -30,5 +32,14 @@ public class SearchString {
             }
         }
         return '\0';
+    }
+
+    static int searchRange(String name, char target) {
+        for (int i = 2; i < 5; i++) {
+            if (target == name.charAt(i)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
