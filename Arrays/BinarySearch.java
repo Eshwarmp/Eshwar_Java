e29525ee718aed70c17f3f4223bb5e80149c44b7
Eshwar <eshwarmp02@gmail.com>
2026-04-25 10:12:41 +0530

    Binary Search Code

---
+++ Arrays/BinarySearch.java
@@ -0,0 +1,31 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class BinarySearch {
+    // Binary Search only applicable when the given is in sorted form.
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = { 1, 2, 3, 4, 5, 6 }; // Make sure the array is sorted in ascending order
+        System.out.println("Enter the elment to search");
+        int target = in.nextInt();
+        int ans = binary(arr, target);
+        System.out.println("The target is present at the index " + ans);
+    }
+
+    static int binary(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;  
+            }
+        }
+        return -1;
+    }
+}
