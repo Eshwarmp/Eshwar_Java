2a7c40e247f9a76511df11cf27aea947251b28ef
Eshwar <eshwarmp02@gmail.com>
2026-06-06 21:46:41 +0530

    Sorting in descending using bubble sort

---
+++ Practice/ReverseBubble.java
@@ -0,0 +1,32 @@
+
+import java.util.Arrays;
+
+public class ReverseBubble {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void reverse(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] > arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
