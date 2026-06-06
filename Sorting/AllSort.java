918216fcdf22ad38846c1db641081bd77fbc7f5b
Eshwar <eshwarmp02@gmail.com>
2026-06-06 13:54:02 +0530

    Code of all sorting techniques till cyclic sort

---
+++ Sorting/AllSort.java
@@ -0,0 +1,82 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class AllSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 1, 4, 3, 2 };
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr) + " -> Bubble Sort");
+        selection(arr);
+        System.out.println(Arrays.toString(arr) + " -> Selection Sort");
+        insertion(arr);
+        System.out.println(Arrays.toString(arr) + " -> Insertion Sort");
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr) + " -> Cyclic Sort");
+    }
+    
+    static void bubbleSort(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+    
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int max = getMax(arr, 0, last);
+            swap(arr, max, last);
+        }
+    }
+
+    static void insertion(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    
+    static void cyclic(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i] - 1;
+            if (arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            }
+            else {
+                i++;
+            }
+        }
+    }
+
+    static int getMax(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
