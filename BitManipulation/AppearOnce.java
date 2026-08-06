525ab0a1ce6623e7ddb33f0e099b3208176d106f
Eshwar <eshwarmp02@gmail.com>
2026-08-06 13:40:34 +0530

    Find the number that is appearing only once in the array

---
+++ BitManipulation/AppearOnce.java
@@ -0,0 +1,12 @@
+package BitManipulation;
+
+public class AppearOnce {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 2, 3,3, 4,1 };
+        int ans = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ans = ans ^ arr[i];
+        }
+        System.out.println(ans);
+    }
+}
