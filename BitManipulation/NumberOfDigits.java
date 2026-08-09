716a2d7d7fbd532c9513e889fad0fdf171bd4391
Eshwar <eshwarmp02@gmail.com>
2026-08-09 23:56:12 +0530

    Number of digits of number n in base b:

---
+++ BitManipulation/NumberOfDigits.java
@@ -0,0 +1,10 @@
+package BitManipulation;
+
+public class NumberOfDigits {
+    public static void main(String[] args) {
+        int n = 4;
+        int base = 2;
+        int ans = (int) ((Math.log(n)) / Math.log(base) + 1);
+        System.out.println(ans);
+    }
+}
