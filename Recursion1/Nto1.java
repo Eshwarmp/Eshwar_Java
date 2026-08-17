a6c0315934140b591c09a2c9dd90fcec78f57e00
Eshwar <eshwarmp02@gmail.com>
2026-08-17 17:04:38 +0530

    Printing numbers from N to 1 using recursion:

---
+++ Recursion1/Nto1.java
@@ -0,0 +1,16 @@
+package Recursion1;
+
+public class Nto1 {
+    public static void main(String[] args) {
+        int n = 5;
+        print(n);
+    }
+
+    public static void print(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.print(n + " ");
+        print(n-1);
+    }
+}
