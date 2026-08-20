afb71ae00321ea8e9fcf443664ceb38e0225f8ea
Eshwar <eshwarmp02@gmail.com>
2026-08-20 23:33:32 +0530

    Modified file of Bubble Sort:

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int[] arr = { 5, 4, 8, 3, 6 };
+        int[] arr = { 4,5,2,1,6};
         bubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
